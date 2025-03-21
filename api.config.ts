affcc3f1deb139131157031def8db2ad25f29e67
Jawil <xjh53047@gmail.com>
2025-03-21 18:14:41 +0800

    feat: update api.config.ts

--- api.config.ts
+++ api.config.ts
@@ -5,12 +5,15 @@ export default defineConfig({
     {
       swaggerUrl: '',
       outputDir: '/src/api',
+      urlPrefix: '',
       enable: true,
       ignore: /\/test\//,
     },
   ],
   httpTpl: 'import type { UseFetchOptions } from \'@vueuse/core\'',
   apiBody: ({ url, method, summary, name, formDataStr, outputInterface, pstr1, pstr2 }) => {
+    pstr1 = `${pstr1?.replace(/(data\??: )/, '$1MaybeRef<')}>`
+    url = url.replace(/\{data/, '{unref(data)')
     return `
             /** ${summary || '无注释'} */
             export function ${name}(${pstr1}, useFetchOptions?: UseFetchOptions) {
